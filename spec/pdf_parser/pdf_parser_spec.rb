0ad56c239d7da5dcf798e55694d00b81c8e112c9
Pavel Lobashov <shockwavenn@gmail.com>
2015-04-13 18:33:31 +0300

    fix test for rspec 3 syntax

--- spec/pdf_parser/pdf_parser_spec.rb
+++ spec/pdf_parser/pdf_parser_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require_relative '../../Helpers/pdf_parser'
+require_relative '../../parsers/pdf_parser'
 
 include PdfParser
 
@@ -7,21 +7,21 @@ describe 'Check PDF parser' do
 
   it 'Check PDF parser | Text without spaces' do
     pdf_info = parse_pdf('PDFText', $*.first.gsub('/pdf_parser_spec.rb', ''))
-    pdf_info[:pages].first[:text].should == 'Text'
+    expect(pdf_info[:pages].first[:text]).to eq('Text')
   end
 
   it 'Check PDF parser | Text with spaces' do
     pdf_info = parse_pdf('PDFTextWithSpaces', $*.first.gsub('/pdf_parser_spec.rb', ''))
-    pdf_info[:pages].first[:text].should == 'Text with spaces'
+    expect(pdf_info[:pages].first[:text]).to eq('Text with spaces')
   end
 
   it 'Check PDF parser | Text with not custom font' do
     pdf_info = parse_pdf('PDFNotCustomFont', $*.first.gsub('/pdf_parser_spec.rb', ''))
-    pdf_info[:pages].first[:fonts].should == 'Times New Roman'
+    expect(pdf_info[:pages].first[:fonts]).to eq('Times New Roman')
   end
 
   it 'Check PDF parser | Text with not custom page size' do
     pdf_info = parse_pdf('PDFNotCustomPageSize', $*.first.gsub('/pdf_parser_spec.rb', ''))
-    pdf_info[:page_size].should == 'B5'
+    expect(pdf_info[:page_size]).to eq('B5')
   end
 end
