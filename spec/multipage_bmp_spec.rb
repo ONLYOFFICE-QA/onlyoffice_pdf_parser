a16bf19b921e0e5c3013335ed9e04361a00137e5
Pavel Lobashov <shockwavenn@gmail.com>
2020-03-12 18:59:46 +0300

    Fix rubocop `RSpec/PredicateMatcher` cop

--- spec/multipage_bmp_spec.rb
+++ spec/multipage_bmp_spec.rb
@@ -10,6 +10,6 @@ describe 'Multipage Bmp' do
 
   it 'contain_pattern for multipage doc' do
     pdf_info = OnlyofficePdfParser::PdfParser.parse('spec/pdf_examples/multipage.pdf')
-    expect(pdf_info.contain_pattern?('spec/pdf_examples/pdf_gridlines_pattern.bmp')).to be_falsey
+    expect(pdf_info).not_to be_contain_pattern('spec/pdf_examples/pdf_gridlines_pattern.bmp')
   end
 end
