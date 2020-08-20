4d446ca4c85d30757684fdef9672e0d2bafb9f3f
Pavel Lobashov <shockwavenn@gmail.com>
2020-08-20 19:05:13 +0300

    Reduce metrics

--- spec/onlyoffice_pdf_parser/bmp_image/get_sub_image_spec.rb
+++ spec/onlyoffice_pdf_parser/bmp_image/get_sub_image_spec.rb
@@ -4,34 +4,26 @@ require 'spec_helper'
 
 describe OnlyofficePdfParser::BmpImage, '#get_sub_image' do
   it 'Check Sub Image horizontal rectangle square pattern' do
-    bmp = described_class.new
+    bmp = described_class.new(nil, height: 3, width: 5)
     bmp.pixels = [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14]]
-    bmp.height = 3
-    bmp.width = 5
     expect(bmp.get_sub_image(OnlyofficePdfParser::CursorPoint.new(1, 1), 2, 2).pixels).to eq([[6, 7], [11, 12]])
   end
 
   it 'Check Sub Image horizontal rectangle rectangle pattern' do
-    bmp = described_class.new
+    bmp = described_class.new(nil, height: 3, width: 5)
     bmp.pixels = [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14]]
-    bmp.height = 3
-    bmp.width = 5
     expect(bmp.get_sub_image(OnlyofficePdfParser::CursorPoint.new(1, 1), 1, 2).pixels).to eq([[6], [11]])
   end
 
   it 'Check Sub Image vertical rectangle square pattern' do
-    bmp = described_class.new
+    bmp = described_class.new(nil, height: 5, width: 3)
     bmp.pixels = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 13, 14]]
-    bmp.height = 5
-    bmp.width = 3
     expect(bmp.get_sub_image(OnlyofficePdfParser::CursorPoint.new(1, 1), 2, 2).pixels).to eq([[4, 5], [7, 8]])
   end
 
   it 'Check Sub Image vertical rectangle rectangle pattern' do
-    bmp = described_class.new
+    bmp = described_class.new(nil, height: 3, width: 5)
     bmp.pixels = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 13, 14]]
-    bmp.height = 5
-    bmp.width = 3
     expect(bmp.get_sub_image(OnlyofficePdfParser::CursorPoint.new(1, 1), 1, 2).pixels).to eq([[4], [7]])
   end
 end
