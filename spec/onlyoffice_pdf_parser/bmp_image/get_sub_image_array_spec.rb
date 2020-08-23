b7613ff205d91c4ada76e6b9cfc49b2282e67a6b
Pavel Lobashov <shockwavenn@gmail.com>
2020-08-23 12:40:58 +0300

    Reduce metrics

--- spec/onlyoffice_pdf_parser/bmp_image/get_sub_image_array_spec.rb
+++ spec/onlyoffice_pdf_parser/bmp_image/get_sub_image_array_spec.rb
@@ -18,21 +18,18 @@ describe OnlyofficePdfParser::BmpImage, '#get_sub_image_array' do
   it 'Find font name canvas pattern in spreadsheet' do
     file = described_class.new('spec/bmp_image_examples/SE_screenshot.bmp')
     pattern = 'spec/bmp_image_examples/SE_font_name.bmp'
-    a = file.get_sub_image_array(pattern)
-    expect(a).to eq([OnlyofficePdfParser::CursorPoint.new(136, 37)])
+    expect(file.get_sub_image_array(pattern)).to eq([OnlyofficePdfParser::CursorPoint.new(136, 37)])
   end
 
   it 'Find left and top canvas part pattern in spreadsheet' do
     file = described_class.new('spec/bmp_image_examples/SE_screenshot.bmp')
     pattern = 'spec/bmp_image_examples/SE_top_left_edge_canvas.bmp'
-    a = file.get_sub_image_array(pattern)
-    expect(a).to eq([OnlyofficePdfParser::CursorPoint.new(39, 111)])
+    expect(file.get_sub_image_array(pattern)).to eq([OnlyofficePdfParser::CursorPoint.new(39, 111)])
   end
 
   it 'Find left and top canvas part pattern without show row names' do
     file = described_class.new('spec/bmp_image_examples/SE_screenshot.bmp')
     pattern = 'spec/bmp_image_examples/SE_top_left_edge_hide_headlines.bmp'
-    a = file.get_sub_image_array(pattern)
-    expect(a).to eq([OnlyofficePdfParser::CursorPoint.new(38, 86)])
+    expect(file.get_sub_image_array(pattern)).to eq([OnlyofficePdfParser::CursorPoint.new(38, 86)])
   end
 end
