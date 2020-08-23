3e8987af6d1717b58dbcb656582ce89ddf41d587
Pavel Lobashov <shockwavenn@gmail.com>
2020-08-23 13:17:01 +0300

    Store gem name in const and use it from there

---
+++ lib/onlyoffice_pdf_parser/name.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module OnlyofficePdfParser
+  # Module with name of Gem
+  module Name
+    # @return [String] name of gem
+    STRING = 'onlyoffice_pdf_parser'
+  end
+end
